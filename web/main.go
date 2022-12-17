9ad00b5b6731639c0dedfc89a905df99b49798a2
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-17 11:34:41 +0330

    start main function

--- web/main.go
+++ web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 	"text/template"
 )
 
@@ -22,10 +24,28 @@ type config struct {
 	}
 }
 
+// reciever for the various parts of app
 type application struct {
 	config        config
 	infoLog       *log.Logger
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
 	version       string
+}
+
+func main() {
+	var cfg config
+
+	// enter cmd into config variable by port
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application enviornment {development|production}")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+
+	flag.Parse()
+
+	// getting secret key for credit card info by clients
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
 }
