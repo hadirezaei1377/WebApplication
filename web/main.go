f1860aafa86600548e4c5fb22e5de4f2acc15d76
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-17 11:22:40 +0330

    type definition

--- web/main.go
+++ web/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+	"text/template"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+// configuration info for app
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
