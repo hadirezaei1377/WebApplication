149428e8614230be6ba687bf3eff6520f72639e8
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-12-18 22:33:16 +0330

    change handler

--- web/handlers.go
+++ web/handlers.go
@@ -5,5 +5,9 @@ import "net/http"
 // a function by pointer to application for access to app config
 // call virtual terminal , its a handler
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the handler")
-}
+	// app.infoLog.Println("Hit the handler")
+	// instead of showing in terminal by above code, render the template
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+// refer ro routes file and put it there
